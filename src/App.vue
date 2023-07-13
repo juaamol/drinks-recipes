7d4432237cbd650301a574c0e68a39870c9f8229
juaamol <jambulud@gmail.com>
2023-07-13 22:15:16 +0200

    add modal

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import SearchHeader from './components/SearchHeader.vue'
+import Modal from './components/Modal.vue'
 </script>
 
 <template>
@@ -8,6 +9,8 @@ import SearchHeader from './components/SearchHeader.vue'
   <main class="container mx-auto mt-10">
     <RouterView />
   </main>
+
+  <Modal />
 </template>
 
 <style scoped></style>
