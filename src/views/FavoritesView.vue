dc26b3d799ceb8328753bdc3d0b922cc5051a3c5
juaamol <jambulud@gmail.com>
2023-07-14 14:51:59 +0200

    favorites placeholder

--- src/views/FavoritesView.vue
+++ src/views/FavoritesView.vue
@@ -7,7 +7,10 @@ const favorites = useFavoritesStore()
 
 <template>
   <h1 class="text-6xl font-extrabold">Favorites</h1>
-  <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 my-10 gap-10">
+  <p v-if="favorites.recipes.length === 0" class="my-10 text-center text-2xl">
+    There are no favorites
+  </p>
+  <div v-else class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 my-10 gap-10">
     <DrinkRecipe :recipe="recipe" v-for="recipe in favorites.recipes" :key="recipe.idDrink" />
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,13 +2,14 @@
 import DrinkRecipe from '../components/DrinkRecipe.vue'
 import { useDrinksStore } from '../stores/drinks'
 
-const store = useDrinksStore()
+const drinks = useDrinksStore()
 </script>
 
 <template>
   <h1 class="text-6xl font-extrabold">Recipes</h1>
-  <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 my-10 gap-10">
-    <DrinkRecipe :recipe="recipe" v-for="recipe in store.recipes" :key="recipe.idDrink" />
+  <p v-if="drinks.recipes.length === 0" class="my-10 text-center text-2xl">There are no results</p>
+  <div v-else class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 my-10 gap-10">
+    <DrinkRecipe :recipe="recipe" v-for="recipe in drinks.recipes" :key="recipe.idDrink" />
   </div>
 </template>
 
